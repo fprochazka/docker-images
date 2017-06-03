594983f8f4454fd1d3ef0adc06409e42b7518144
Filip Procházka <filip@prochazka.su>
2017-06-03 15:17:44 +0200

    executable PHP

---
+++ php71/run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Run PHP in a container
+#
+
+set -e
+
+VERSION="7.1"
+IMAGE="fprochazka/php:$VERSION-exec"
+
+DOCKER_VOLUMES=""
+DOCKER_ENVIRONMENT=""
+
+# Setup volume mounts for compose config and context
+if [ "$(pwd)" != '/' ]; then
+    DOCKER_VOLUMES="$DOCKER_VOLUMES -v $(pwd):$(pwd)"
+fi
+if [ -n "$HOME" ]; then
+    DOCKER_VOLUMES="$DOCKER_VOLUMES -v $HOME:$HOME"
+    PHP_HOME="$HOME/.composer"
+fi
+if [ -n "$SSH_AUTH_SOCK" ]; then
+	DOCKER_VOLUMES="$DOCKER_VOLUMES -v $SSH_AUTH_SOCK:/ssh-agent"
+	DOCKER_ENVIRONMENT="$DOCKER_ENVIRONMENT -e SSH_AUTH_SOCK=/ssh-agent"
+fi
+
+if [[ -f "$1" || -d "$1" ]]; then
+    DOCKER_VOLUMES="$DOCKER_VOLUMES -v $1:$1"
+fi
+
+if [ -z "$PHP_USER_NAME" ]; then
+	export PHP_USER_NAME=$(id -un)
+fi
+if [ -z "$PHP_USER_UID" ]; then
+	export PHP_USER_UID=$(id -u)
+fi
+if [ -z "$PHP_USER_GID" ]; then
+	export PHP_USER_GID=$(id -g)
+fi
+
+PHP_USER="-e PHP_USER_NAME -e PHP_USER_UID -e PHP_USER_GID -e PHP_HOME";
+
+# Only allocate tty if we detect one
+if [ -t 1 ]; then
+    DOCKER_RUN_OPTIONS="-t"
+fi
+if [ -t 0 ]; then
+    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -i"
+fi
+
+exec docker run --rm -u root $DOCKER_RUN_OPTIONS $DOCKER_VOLUMES $DOCKER_ENVIRONMENT $PHP_USER -w "$(pwd)" $IMAGE php "$@"
